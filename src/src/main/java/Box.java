d864a7222c49225445029c21cb271b050b774a0a
AbiPetheram <abi.gill97@gmail.com>
2024-07-26 11:37:03 +0100

    feat: add hasSameName method to box

--- src/src/main/java/Box.java
+++ src/src/main/java/Box.java
@@ -1,8 +1,10 @@
 public class Box<T> {
     T value;
+    private String name;
 
-    public Box(T value) {
+    public Box(T value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public T getValue() {
@@ -13,11 +15,26 @@ public class Box<T> {
         this.value = value;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public <V> T castValue(V value) {
         try {return (T) value;
         } catch (ClassCastException e) {
             System.out.println("Failed: cannot cast this value");
         }
         return null;
+    }
+
+    public boolean hasSameName(Box<?> box){
+        if(this.name.equals(box.name)){
+            return true;
+        }
+        return false;
     }
 }
--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box<>(5);
+        Box box = new Box<>(5, "My Box");
         box.castValue(5);
         System.out.println(box.castValue(2));
         System.out.println(box.castValue("Result: 2"));
@@ -19,5 +19,8 @@ public class Main {
 
         box.castValue(fruit);
         System.out.println(box.castValue(fruit));
+
+        Box newBox = new Box<Integer>(2, "My Box");
+        System.out.println(box.hasSameName(newBox));
     }
 }
